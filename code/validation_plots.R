df048d15aa71ec445d5fec9a46de047a2223ce91
lucialayr <lucia.sophie.layritz@posteo.de>
2026-02-06 17:44:49 -0800

    redid validation figure

--- code/validation_plots.R
+++ code/validation_plots.R
@@ -1,5 +1,5 @@
-setwd("~/Desktop/PhD/borealRecovery")
-source("code/utils.R")
+library(here)
+source(here("code", "utils.R"))
 
 library(tidyverse)
 library(terra)
@@ -12,9 +12,9 @@ library(scico)
 
 validation_A_plot = function() {
   
-  shp = st_read("data/final/shp/validation_A.shp")
+  shp = st_read(here("data", "final", "shp", "validation_A.shp"))
   
-  study_region = st_read("data/external/vegetation_ssp585_d0.003_fpc_30years2100.shp") %>%
+  study_region = st_read(here("data", "external", "vegetation_ssp585_d0.003_fpc_30years2100.shp")) %>%
     st_make_valid() %>%
     st_union() %>%
     st_geometry() %>%
@@ -36,7 +36,7 @@ validation_A_plot = function() {
       theme(legend.position = "bottom",
             legend.direction = "horizontal",
             legend.title.position = "top") +
-      guides(fill = guide_legend(ncol = 2, byrow = T)))
+      guides(fill = guide_legend(ncol = 1, byrow = T)))
   
   return(p1)
   
@@ -44,7 +44,8 @@ validation_A_plot = function() {
 
 validation_B_plot = function() {
   
-  df = read_csv("data/final/validation_B.csv")
+  df = read_csv(here("data", "final", "validation_B.csv")) %>%
+    mutate(PFT = ifelse(PFT == "Shrubs, Herb. & Sparse Veg.", "Shrubs & Herb.", PFT))
   
   df$age_bin = factor(df$age_bin, levels = c("02-05", "06-09", "10-19", "20-29", 
                                              "30-39", "40-49", "50-69", "50-59"))
@@ -52,26 +53,27 @@ validation_B_plot = function() {
   df$ecoregion = factor(df$ecoregion, levels = str_to_title(c("ALASKA BOREAL INTERIOR", "TAIGA CORDILLERA",
                                                               "TAIGA PLAIN", "TAIGA SHIELD", 
                                                               "BOREAL CORDILLERA", "BOREAL PLAIN")))
+
   
   (p2 = ggplot() +
       coord_cartesian(clip = "off") +
-      facet_wrap(~ecoregion, ncol = 2) +
+      facet_grid(rows = vars(PFT), cols = vars(ecoregion), scales = 'free_y') +
+      geom_hline(yintercept = 0, color = "black", linewidth = .3) +
       geom_line(data = df, aes( x = age_bin, y = value, group = interaction(age_bin, ecoregion, PFT)), linewidth = .25, color = "black") + 
-      geom_point(data = df, aes(x = age_bin, y = value, fill = PFT, shape = dataset), size = 3, color = "black") +  # Adding color for outline
+      geom_point(data = df, aes(x = age_bin, y = value, fill = ecoregion, shape = dataset), size = 3, color = "black") +  # Adding color for outline
       scale_x_discrete(name = "Year since disturbance") +
       scale_y_continuous(name = "Relative share of vegetation cover", 
-                         breaks = c(0, 0.5, 1), limits = c(0,1), expand = c(0,0)) +
-      scale_fill_manual(name = "Vegetation class",
-                        values = c("Decid. Forest" = "#E69F00",  "Everg. Forest" = "#0072B2",   
-                                   "Shrubs, Herb. & Sparse Veg." = "#009E73")) +
+                         breaks = c(0.25, 0.5, 0.75),  labels = c('0.25', "", '0.75'), limits = c(0, 1), expand = c(0,0)) +
+      scale_fill_scico_d(name = "Ecoregions", palette = "navia", begin = .1, direction = 1) +
       scale_shape_manual(name = "Dataset", values = c("Observations" = 25,  "LPJ-GUESS" = 24)) +
       theme(legend.position = "bottom",
             legend.direction = "horizontal",
             legend.box = 'horizontal',
             legend.title.position = "top",
+            strip.text.x = element_blank(),
             legend.justification = c(1, 0),
-            axis.text.x = element_text(angle = 90)) +
-      guides(fill = guide_legend(override.aes=list(shape=24, size = 4)),
+            axis.text.x = element_text(angle = 90, size = 11, vjust = .5)) +
+      guides(fill = 'none',
              shape = guide_legend(override.aes = list(size = 4))))
   
   return(p2)
@@ -86,15 +88,20 @@ validation_plot = function() {
   
   legend = get_legend(validation_B)
   
-  plot_grid(validation_A, validation_B, labels = c("(a)", "(b)"), rel_widths = c(0.7, 1), align = "hv", axis = "b")
+  plot_grid(plot_grid(validation_A, legend, ncol = 1, rel_heights = c(1, 0.1)),
+            validation_B + theme(legend.position = 'none'), 
+            labels = c("(a)", "(b)"), rel_widths = c(0.625, 1),  axis = "b")
   
-  ggsave("plots/recovery_validation.pdf", width = 10)
-  ggsave("plots/recovery_validation.png", width = 10,  height = 6.5, dpi=300)
+  ggsave(here("plots", "recovery_validation.pdf"), width = 11)
+  ggsave(here("plots", "recovery_validation.png"), width = 10, height = 6.5, dpi = 300)
   
 }
 
 validation_plot()
 
+
+validation_B_plot() +
+  scale_y_log10()
 
 
 
